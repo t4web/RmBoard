6bdb571edd1aaecd240d8e7df304ea605c71c59d
maxgu <maxgu@t4web.com.ua>
2015-02-14 11:46:21 +0200

    little clearing

--- js/template/login-form.js
+++ js/template/login-form.js
@@ -10,16 +10,6 @@ define([], function() {
                             <textarea class="form-control" id="auth-token"></textarea>\n\
                         </div>\n\
                     </div>\n\
-                    <!--<div class="modal-body">\n\
-                        <div class="form-group">\n\
-                            <label for="recipient-name" class="control-label">Recipient:</label>\n\
-                            <input type="text" class="form-control" id="recipient-name">\n\
-                        </div>\n\
-                        <div class="form-group">\n\
-                            <label for="message-text" class="control-label">Message:</label>\n\
-                            <input type="text" class="form-control" id="recipient-name">\n\
-                        </div>\n\
-                    </div>-->\n\
                     <div class="modal-footer">\n\
                         <button type="button" class="btn btn-primary btn-block" id="login-btn">Login me in</button>\n\
                     </div>\n\
